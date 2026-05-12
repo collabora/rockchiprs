f9d8ed32974a8647b3d6ce544e59f0d6f56cb725
Sjoerd Simons <sjoerd@collabora.com>
2026-05-12 21:47:21 +0200

    tests: Add write test for block io
    Add a trivial write test for block io, simply writing bytes linearly one
    by one and confirm the flash content matches.

--- rockusb/tests/blockio.rs
+++ rockusb/tests/blockio.rs
@@ -1,5 +1,6 @@
 use rockusb::protocol::SECTOR_SIZE;
 use rockusb_mock::{MockDevice, MockDeviceConfig};
+use std::io::Write;
 
 #[test]
 fn erase_lba() {
@@ -22,4 +23,24 @@ fn erase_lba() {
             .iter()
             .all(|byte| *byte == 0x11)
     );
+}
+
+#[test]
+fn simple_linear_write() {
+    const SECTORS: usize = 4096;
+    let mut mock = MockDevice::new(MockDeviceConfig {
+        flash: vec![0x0; SECTORS * SECTOR_SIZE as usize],
+        ..MockDeviceConfig::default()
+    })
+    .unwrap();
+    let device = mock.device();
+    let mut io = device.io().unwrap();
+    for i in 0..(SECTORS * SECTOR_SIZE as usize) {
+        io.write_all(&[(i & 0xff) as u8]).unwrap();
+    }
+
+    let flash = mock.flash();
+    for (pos, b) in flash.iter().enumerate() {
+        assert_eq!((pos & 0xff) as u8, *b, "unexpected value, pos {pos}");
+    }
 }
